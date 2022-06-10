fd79d261f3717ffef502bf6cad632cc01ba55aff
Methan <methanroyal@gmail.com>
2022-06-10 21:57:36 +0530

    Testing

--- tests.py
+++ tests.py
@@ -5,12 +5,16 @@ import time
 import zipfile
 import yaml
 import shogi
+import engine_ctrl
+import threading
 import shogi.KIF as kif
 import shutil
 import importlib
+import random
+from shutil import copyfile
+import importlib
+copyfile('./test/lishogi.py', 'lishogi.py')
 lishogi_bot = importlib.import_module("lishogi-bot")
-
-TOKEN = "Ao7zQxDqoBhE7tpG"
 
 
 def test_nothing():
@@ -26,67 +30,109 @@ def download_yo():
     shutil.copyfile(f"YaneuraOu_NNUE-tournament-clang++-sse42.exe", f"yo.exe")
 
 
+class RandomEngine:
+    def play(self, board, *args):
+        return random.choice(board.legal_moves)
+
+
 def run_bot(CONFIG, logging_level):
     lishogi_bot.logger.info(lishogi_bot.intro())
-    li = lishogi_bot.lishogi.Lishogi(CONFIG["token"], CONFIG["url"], lishogi_bot.__version__, logging_level)
+    li = lishogi_bot.lishogi.Lishogi(CONFIG["token"], CONFIG["url"], lishogi_bot.__version__)
 
     user_profile = li.get_profile()
     username = user_profile["username"]
     is_bot = user_profile.get("title") == "BOT"
-    lishogi_bot.logger.info(f"Welcome BOT {username}!")
+    lishogi_bot.logger.info("Welcome {}!".format(username))
 
     if not is_bot:
         is_bot = lishogi_bot.upgrade_account(li)
 
     if is_bot:
-        games = li.get_ongoing_games()
-        game_ids = list(map(lambda game: game["gameId"], games))
-        for game in game_ids:
-            try:
-                li.abort(game)
-            except:
-                pass
-            time.sleep(2)
-        while li.get_ongoing_games():
-            time.sleep(60)
-        game_id = li.challenge_ai()["id"]
-        time.sleep(2)
-        games = li.get_ongoing_games()
-        game_ids = list(map(lambda game: game["gameId"], games))
-        for game in game_ids:
-            if game != game_id:
-                try:
-                    li.abort(game)
-                except:
-                    pass
-                time.sleep(2)
+        engine_factory = lishogi_bot.partial(lishogi_bot.engine_wrapper.create_engine, CONFIG)
 
         @pytest.mark.timeout(300)
         def run_test():
-            lishogi_bot.start(li, user_profile, CONFIG, logging_level, None, one_game=True)
-            response = requests.get(f"https://lishogi.org/game/export/{game_id}")
-            response = response.content.decode()
-            parser = kif.Parser()
-            summary = parser.parse_str(response)[0]
-            starting = "lishogi" in summary["names"][1]
-            board = shogi.Board()
-            for move in summary["moves"]:
-                board.push_usi(move)
-            win = board.turn == (shogi.WHITE if starting else shogi.BLACK) and board.is_game_over()
-            assert win
+
+            def test_thr():
+                open('events.txt', 'w').close()
+
+                board = shogi.Board()
+                btime = 60
+                wtime = 60
+
+                with open('states.txt', 'w') as file:
+                    file.write('\n60,60')
+
+                engine = engine_ctrl.Engine('yo.exe')
+                engine.usi()
+                engine.isready()
+                engine.setoption('Skill Level', 0)
+                engine.setoption('Move Overhead', 1000)
+
+                while True:
+                    if board.is_game_over():
+                        with open('events.txt', 'w') as file:
+                            file.write('end')
+                        break
+
+                    if len(board.move_stack) % 2 == 0:
+                        with open('states.txt') as states:
+                            state = states.read().split('\n')
+                        moves = state[0]
+                        if not board.move_stack:
+                            move, _ = engine.go('startpos', '', movetime=1000)
+                        else:
+                            start_time = time.perf_counter_ns()
+                            move, _ = engine.go('startpos', moves.split(), btime=int((btime - 2) * 1000), binc=2000)
+                            end_time = time.perf_counter_ns()
+                            btime -= (end_time - start_time) / 1e9
+                            btime += 2
+                        board.push_usi(move)
+
+                        with open('states.txt') as states:
+                            state = states.read().split('\n')
+                        state[0] += ' ' + move
+                        state = '\n'.join(state)
+                        with open('states.txt', 'w') as file:
+                            file.write(state)
+
+                    else:  # lishogi-bot move
+                        start_time = time.perf_counter_ns()
+                        while True:
+                            with open('states.txt') as states:
+                                state2 = states.read()
+                            time.sleep(0.001)
+                            if state != state2:
+                                break
+                        with open('states.txt') as states:
+                            state2 = states.read()
+                        end_time = time.perf_counter_ns()
+                        if len(board.move_stack) > 1:
+                            wtime -= (end_time - start_time) / 1e9
+                            wtime += 2
+                        move = state2.split('\n')[0].split(' ')[-1]
+                        board.push_usi(move)
+
+                    time.sleep(0.001)
+                    with open('states.txt') as states:
+                        state = states.read().split('\n')
+                    state[1] = f'{wtime},{btime}'
+                    state = '\n'.join(state)
+                    with open('states.txt', 'w') as file:
+                        file.write(state)
+
+                engine.quit()
+                win = board.is_checkmate() and board.turn == shogi.BLACK
+                assert win
+
+            thr = threading.Thread(target=test_thr)
+            thr.start()
+            lishogi_bot.start(li, user_profile, engine_factory, CONFIG, logging_level, None, one_game=True)
+            thr.join()
 
         run_test()
     else:
-        lishogi_bot.logger.error(f"{username} is not a bot account. Please upgrade it to a bot account!")
-    games = li.get_ongoing_games()
-    game_ids = list(map(lambda game: game["gameId"], games))
-    for game in game_ids:
-        try:
-            li.abort(game)
-        except:
-            pass
-        time.sleep(2)
-
+        lishogi_bot.logger.error("{} is not a bot account. Please upgrade it to a bot account!".format(user_profile["username"]))
 
 def test_bot():
     logging_level = lishogi_bot.logging.INFO  # lishogi_bot.logging_level.DEBUG
@@ -96,7 +142,7 @@ def test_bot():
     lishogi_bot.logger.info("Downloaded YaneuraOu for NNUE")
     with open("./config.yml.default") as file:
         CONFIG = yaml.safe_load(file)
-    CONFIG["token"] = TOKEN
+    CONFIG["token"] = ''
     CONFIG["engine"]["dir"] = "./"
     CONFIG["engine"]["name"] = "yo.exe"
     CONFIG["engine"]["usi_options"]["BookFile"] = "no_book"
